d142823025ca0f1b7018184881e3af982acbee9a
Supermegatom <tom@digitalcoin.tech>
2018-05-22 21:52:06 +0200

    replacing "dash" string with "digitalcoin"

--- lib/dashlib.py
+++ lib/dashlib.py
@@ -251,7 +251,7 @@ def did_we_vote(output):
     err_msg = ''
 
     try:
-        detail = output.get('detail').get('dash.conf')
+        detail = output.get('detail').get('digitalcoin.conf')
         result = detail.get('result')
         if 'errorMessage' in detail:
             err_msg = detail.get('errorMessage')
